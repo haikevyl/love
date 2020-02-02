5c51fdf75a602fd488759962c3da66df0b23f476
= <=>
2020-02-02 15:00:24 +0700

    upload

--- js/app.js
+++ js/app.js
@@ -1,12 +1,69 @@
-$(window).on("load", function(){
-    setTimeout(function(){
-        playPause();
-    }, 10000)
-});
-
 $(document).ready(function(){
+    var musicTracker = 'noMusic';
+    var audios = [];
+
+    $(".audio-list li a").each(function(){
+            var load = new  Audio($(this).attr("href"));
+        load.load();
+        load.addEventListener('ended',function(){
+            forward();
+        });
+        audios.push(load);
+    });
+
+    var activeTrack = 0;
+    var playPause = function() {
+        if (musicTracker == 'noMusic') {
+            audios[activeTrack].play();
+            musicTracker = 'playMusic';
+        } else {
+            audios[activeTrack].pause();
+            musicTracker = 'noMusic';
+        }
+    };
+
+    var stop = function() {
+        if (musicTracker == 'playMusic') {
+                audios[activeTrack].pause();
+                audios[activeTrack].currentTime = 0;
+                audios[activeTrack].play();
+        } else {
+            audios[activeTrack].currentTime = 0;
+        }
+    };
+
+    var forward = function(){
+        function increment(){
+             if (activeTrack < audios.length - 1)
+                   activeTrack++;
+           else activeTrack = 0;
+        }
+          if (musicTracker == 'playMusic') {
+             audios[activeTrack].pause();
+           increment();
+             audios[activeTrack].play();
+        } else {
+          increment();
+        }
+    };
+    
+    var backward = function(){
+        function decrement(){
+             if (activeTrack > 0)
+                   activeTrack--;
+           else activeTrack = audios.length -1;
+        }
+          if (musicTracker == 'playMusic') {
+             audios[activeTrack].pause();
+           decrement();
+             audios[activeTrack].play();
+        } else {
+          decrement();
+        }
+    };
+
     var text = [
-        "Chèo Vợ Yêu,",
+        "Chào Vợ Yêu,",
         "Vợ là Người Con Gái Chồng Yêu Nhất :))",
         "Vợ Đáng Yêu Nhất !",
         "Vợ Cũng yêu Chồng Nhất :) !",
@@ -47,17 +104,6 @@ $(document).ready(function(){
         heart();
     }, 500);
 
-    setTimeout(() => {
-        $(".title").css({
-            "visibility" : "hidden",
-            "opacity" : "0"
-        });
-        setTimeout(function(){
-            $('.text').fadeIn("slow");
-            typeWriter();
-        }, 400);
-    }, 16100);
-
     $("#action-media-control").click(function(){
         $(".media-control").toggleClass("media-control-active");
         if($(this).html() == '<i class="fas fa-chevron-left"></i>'){
@@ -81,4 +127,20 @@ $(document).ready(function(){
     $('#next').click(function(){
         forward();
     });
+    setTimeout(function(){
+        playPause();
+    }, 10000);
+
+    setTimeout(() => {
+        $(".title").css({
+            "visibility" : "hidden",
+            "opacity" : "0"
+        });
+        setTimeout(function(){
+            $('.text').fadeIn("slow");
+            typeWriter();
+        }, 400);
+    }, 16100);
 })
+
+
